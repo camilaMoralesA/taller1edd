16c72f695e7a6c346ed9694fb9ee2304aec11d84
camilaMoralesA <camila.morales03@alumnos.ucn.cl>
2026-05-03 23:53:54 -0400

    creacion de la lista enlazada

--- data_structures/Lista.h
+++ data_structures/Lista.h
@@ -5,10 +5,24 @@
 #ifndef TALLER_LISTA_H
 #define TALLER_LISTA_H
 
-
+#include "Nodo.h"
+template <typename T>
 
 class Lista {
-
+private:
+    Nodo<T> *head;
+    Nodo<T> *cola;
+public:
+    Lista() {
+        this->head = NULL;
+        this->cola = NULL;
+    }
+    Nodo<T> *getCola() {
+        return this->cola;
+    }
+    Nodo<T> *getHead() {
+        return this->head;
+    }
 };
 
 
