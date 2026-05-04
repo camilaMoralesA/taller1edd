28515ade0ae9ee61c5e5347604799395ef771467
camilaMoralesA <camila.morales03@alumnos.ucn.cl>
2026-05-03 22:14:56 -0400

    creacion de la clase cancion

---
+++ clases/Cancion.cpp
@@ -0,0 +1,5 @@
+//
+// Created by camil on 5/3/2026.
+//
+
+#include "Cancion.h"
---
+++ clases/Cancion.h
@@ -0,0 +1,24 @@
+//
+// Created by camil on 5/3/2026.
+//
+
+#ifndef TALLER_CANCION_H
+#define TALLER_CANCION_H
+#include <string>
+
+
+//id_interno,nombre_cancion,nombre_artista,nombre_album,año,duración_en_segundos,ubicacion_archivo
+
+class Cancion {
+    int id;
+    string nombre ;
+    string autor;
+    string album;
+
+
+
+};
+
+
+
+#endif //TALLER_CANCION_H
